bd2cc84b9373c0f9b5565ecda018afcddf184300
Pian <alifalpian157@gmail.com>
2025-03-04 13:57:29 +0700

    colection component

--- vite-project/src/todolist/TodoList.jsx
+++ vite-project/src/todolist/TodoList.jsx
@@ -1,54 +1,54 @@
-import { TodoMain, Todo } from "./TodoMain.jsx";  
+import { TodoMain, Todo } from "./TodoMain.jsx";
 
 export default function TodoList() {
   const data = [
     {
-      "text": "Learn HTML",
-      "isCompleted": true
+      text: "Learn HTML",
+      isCompleted: true,
     },
     {
-      "text": "Learn CSS",
-      "isCompleted": false
+      text: "Learn CSS",
+      isCompleted: false,
     },
     {
-      "text": "Learn JavaScript",
-      "isCompleted": true
+      text: "Learn JavaScript",
+      isCompleted: true,
     },
     {
-      "text": "Learn React.js",
-      "isCompleted": false
+      text: "Learn React.js",
+      isCompleted: false,
     },
     {
-      "text": "Learn Next.js",
-      "isCompleted": true
+      text: "Learn Next.js",
+      isCompleted: true,
     },
     {
-      "text": "Learn TypeScript",
-      "isCompleted": false
+      text: "Learn TypeScript",
+      isCompleted: false,
     },
     {
-      "text": "Learn Node.js",
-      "isCompleted": true
+      text: "Learn Node.js",
+      isCompleted: true,
     },
     {
-      "text": "Learn Express.js",
-      "isCompleted": false
+      text: "Learn Express.js",
+      isCompleted: false,
     },
     {
-      "text": "Learn MongoDB",
-      "isCompleted": true
+      text: "Learn MongoDB",
+      isCompleted: true,
     },
     {
-      "text": "Learn GraphQL",
-      "isCompleted": false
-    }
-  ]
-  const todos = data.map((todo, index) => (
-    <Todo key={index} {...todo} />
-  ));
-    
+      text: "Learn GraphQL",
+      isCompleted: false,
+    },
+  ];
+  const todos = data.map((todo, index) => <Todo key={index} {...todo} />);
+
   return (
-    <> {/* Parent wrapper */}
+    <>
+      {" "}
+      {/* Parent wrapper */}
       <ul>
         <TodoMain isCompleted={true} text="Learn HTML" isDeleted={true} />
         <TodoMain isCompleted={false} text="Learn CSS" />
@@ -61,7 +61,6 @@ export default function TodoList() {
         <TodoMain isCompleted={true} text="Learn MongoDB" />
         <TodoMain isCompleted={false} text="Learn GraphQL" />
       </ul>
-
       <ul>
         <Todo isCompleted={true} text="Learn HTML" isDeleted={true} />
         <Todo isCompleted={false} text="Learn CSS" />
@@ -74,10 +73,7 @@ export default function TodoList() {
         <Todo isCompleted={true} text="Learn MongoDB" />
         <Todo isCompleted={false} text="Learn GraphQL" />
       </ul>
-
-      <ul>
-        {todos}
-      </ul>
+      <ul>{todos}</ul>
     </>
   );
 }
