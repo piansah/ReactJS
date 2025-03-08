4dfc153ccc5f383b46aeb335279a726eabac1759
Pian <alifalpian157@gmail.com>
2025-03-08 15:23:56 +0700

    Snapshot

--- vite-project/src/form/Counter.jsx
+++ vite-project/src/form/Counter.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
 export default function Counter() {
-  let [count, setCount] = useState(0);
+  let [count, setCounter] = useState(0);
+  
+  console.info(`Render Counter ${count}`);
+
+  function handleClick() {
+    setCounter(count + 3);
+    console.log(count);
+  }
+
   return (
     <div>
-      <button onClick={() => setCount((count) => count + 1)}>Click Count</button>
+      <button onClick={handleClick}>Click Count</button>
       <p>Counter is {count}</p>
     </div>
   )
