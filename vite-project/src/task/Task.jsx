4902570490f0fcf17d71d4bc583a225b76f32568
Pian <alifalpian157@gmail.com>
2025-03-09 15:00:50 +0700

    save

--- vite-project/src/task/Task.jsx
+++ vite-project/src/task/Task.jsx
@@ -2,30 +2,30 @@ import { useState } from "react";
 import { useImmer } from "use-immer";
 
 export default function Task() {
-    const [item, setItem] = useState("");
-    const [list, setList] = useImmer([]);
+  const [item, setItem] = useState("");
+  const [list, setList] = useImmer([]);
 
-    function handleChange(e) {
-        setItem(e.target.value);
-    }
+  function handleChange(e) {
+    setItem(e.target.value);
+  }
 
-    function handleClick(e) {
-        e.preventDefault();
-        setList((draft) => {
-            draft.push(item); // Gunakan draft, bukan list
-        });
-        setItem("");
-    }
+  function handleClick(e) {
+    e.preventDefault();
+    setList((draft) => {
+      draft.push(item); // Gunakan draft, bukan list
+    });
+    setItem("");
+  }
 
-    return (
-        <div>
-            <input type="text" value={item} onChange={handleChange} />
-            <button onClick={handleClick}>Add</button>
-            <ul>
-                {list.map((task, index) => (
-                    <li key={index}>{task}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <input type="text" value={item} onChange={handleChange} />
+      <button onClick={handleClick}>Add</button>
+      <ul>
+        {list.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
