fbcea7eb6b7a8d0f61ec810ccad5d5b9d70a3977
Pian <alifalpian157@gmail.com>
2025-03-10 14:09:19 +0700

    Sharing State

--- vite-project/src/task/Task.jsx
+++ vite-project/src/task/Task.jsx
@@ -1,32 +1,14 @@
-import { useState } from "react";
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm.jsx";
+import TaskList from "./TaskList.jsx";
 
 export default function Task() {
-  const [item, setItem] = useState("");
   const [list, setList] = useImmer([]);
-
-  function handleChange(e) {
-    setItem(e.target.value);
-  }
-
-  function handleClick(e) {
-    e.preventDefault();
-    setList((draft) => {
-      draft.push(item); // Gunakan draft, bukan list
-    });
-    setItem("");
-  }
 
   return (
     <div>
-      <h1>Create Task</h1>
-      <input type="text" value={item} onChange={handleChange} />
-      <button onClick={handleClick}>Add</button>
-      <ul>
-        {list.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+      <TaskForm setList={setList} />
+      <TaskList items={list} />
     </div>
   );
 }
