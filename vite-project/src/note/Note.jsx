cdade6244e3298552daa12d11f55f3d34ea7fc91
Pian <alifalpian157@gmail.com>
2025-03-12 20:21:58 +0700

    checkbox

--- vite-project/src/note/Note.jsx
+++ vite-project/src/note/Note.jsx
@@ -2,29 +2,32 @@ import { useState } from "react";
 
 export default function Note({ note, onChange, onDelete }) {
   const [isEditing, setIsEditing] = useState(false);
-  let component;
 
   function handleChangeText(e) {
     const newNote = { ...note, text: e.target.value };
     onChange(newNote);
   }
 
-  if (isEditing) {
-    component = (
-      <div>
-        <input value={note.text} onChange={handleChangeText} />
-        <button onClick={() => setIsEditing(false)}>Cancel</button>
-      </div>
-    );
-  } else {
-    component = (
-      <div>
-        {note.text}
-        <button onClick={() => setIsEditing(true)}>Edit</button>
-        <button onClick={() => onDelete(note.id)}>Delete</button>
-      </div>
-    );
+  function handleToggleDone() {
+    const updatedNote = { ...note, done: !note.done };
+    onChange(updatedNote);
   }
 
-  return component;
+  return (
+    <div>
+      <input type="checkbox" checked={note.done} onChange={handleToggleDone} />
+      {isEditing ? (
+        <>
+          <input value={note.text} onChange={handleChangeText} />
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </>
+      ) : (
+        <>
+          {note.text}
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => onDelete(note.id)}>Delete</button>
+        </>
+      )}
+    </div>
+  );
 }
