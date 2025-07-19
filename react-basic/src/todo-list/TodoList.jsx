8baa13c89069a0fc1fd8b3cb57b65e75028244c5
Pian <alifalpian157@gmail.com>
2025-07-19 22:17:00 +0700

    Colection Component

--- react-basic/src/todo-list/TodoList.jsx
+++ react-basic/src/todo-list/TodoList.jsx
@@ -2,26 +2,20 @@ import Todo from "./Todo.jsx";
 
 export default function TodoList() {
   const data = [
-    { PropsText: "Learn HTML", isCompleted: true },
-    { PropsText: "Learn CSS", isCompleted: true },
-    { PropsText: "Learn JavaScript", isCompleted: true },
-    { PropsText: "Learn React", isCompleted: false },
-    { PropsText: "Learn PHP", isCompleted: false },
-    { PropsText: "Learn Python", isCompleted: true },
-    { PropsText: "Learn Java", isCompleted: false },
+    { id: 0, PropsText: "Learn HTML", isCompleted: true },
+    { id: 1, PropsText: "Learn CSS", isCompleted: true },
+    { id: 2, PropsText: "Learn JavaScript", isCompleted: true },
+    { id: 3, PropsText: "Learn React", isCompleted: false },
+    { id: 4, PropsText: "Learn PHP", isCompleted: false, isDeleted: true },
+    { id: 5, PropsText: "Learn Python", isCompleted: true, isDeleted: true },
+    { id: 6, PropsText: "Learn Java", isCompleted: false, isDeleted: true },
   ];
 
-  const todos = data.map((item, index) => <Todo key={index} {...item} />);
-
-  return <ul>{todos}</ul>;
+  return (
+    <ul>
+      {data.map((todo) => (
+        <Todo key={todo.id} {...todo} />
+      ))}
+    </ul>
+  );
 }
-
-// <ul>
-//   <Todo PropsText="Belajar React" isCompleted={true} />
-//   <Todo PropsText="Belajar Javascript" isCompleted={true} />
-//   <Todo PropsText="Belajar CSS" isCompleted={true} />
-//   <Todo PropsText="Belajar HTML" isCompleted={false} />
-//   <Todo PropsText="Belajar PHP" isDeleted={true} />
-//   <Todo PropsText="Belajar Python" isDeleted={true} />
-//   <Todo PropsText="Belajar Java" isDeleted={true} />
-// </ul>
