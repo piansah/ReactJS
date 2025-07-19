743520807787ef6e0c47812ebe24f62a853ab002
Pian <alifalpian157@gmail.com>
2025-07-19 20:11:33 +0700

    component colection

--- react-basic/src/todo-list/Todo.jsx
+++ react-basic/src/todo-list/Todo.jsx
@@ -1,13 +1,24 @@
-export default function Todo({ PropsText, isCompleted, isDeleted = false}) {
+export default function Todo({ PropsText, isCompleted, isDeleted = false }) {
   if (isDeleted) {
     return null;
-  } else if (isCompleted) {
+  }
+  //   else if (isCompleted) {
+  //     return (
+  //       <li>
+  //         <del>{PropsText}</del>
+  //       </li>
+  //     );
+  //   }
+  //   else {
+  //     return <li>{PropsText}</li>;
+  //   }
+  else {
     return (
       <li>
-        <del>{PropsText}</del>
+        {/* Ternary Operator */}
+        {/* {isCompleted ? <del>{PropsText}</del> : PropsText} */}
+        {PropsText} {isCompleted && '🚀'}
       </li>
     );
-  } else {
-    return <li>{PropsText}</li>;
   }
 }
--- react-basic/src/todo-list/TodoList.jsx
+++ react-basic/src/todo-list/TodoList.jsx
@@ -1,12 +1,27 @@
 import Todo from "./Todo.jsx";
 
 export default function TodoList() {
-  return (
-    <ul>
-      <Todo PropsText="Belajar React" isCompleted={true} />
-      <Todo PropsText="Belajar Javascript" isDeleted={true} />
-      <Todo PropsText="Belajar CSS" isCompleted={true} />
-      <Todo PropsText="Belajar HTML" isCompleted={false} />
-    </ul>
-  );
+  const data = [
+    { PropsText: "Learn HTML", isCompleted: true },
+    { PropsText: "Learn CSS", isCompleted: true },
+    { PropsText: "Learn JavaScript", isCompleted: true },
+    { PropsText: "Learn React", isCompleted: false },
+    { PropsText: "Learn PHP", isCompleted: false },
+    { PropsText: "Learn Python", isCompleted: true },
+    { PropsText: "Learn Java", isCompleted: false },
+  ];
+
+  const todos = data.map((item, index) => <Todo key={index} {...item} />);
+
+  return <ul>{todos}</ul>;
 }
+
+// <ul>
+//   <Todo PropsText="Belajar React" isCompleted={true} />
+//   <Todo PropsText="Belajar Javascript" isCompleted={true} />
+//   <Todo PropsText="Belajar CSS" isCompleted={true} />
+//   <Todo PropsText="Belajar HTML" isCompleted={false} />
+//   <Todo PropsText="Belajar PHP" isDeleted={true} />
+//   <Todo PropsText="Belajar Python" isDeleted={true} />
+//   <Todo PropsText="Belajar Java" isDeleted={true} />
+// </ul>
