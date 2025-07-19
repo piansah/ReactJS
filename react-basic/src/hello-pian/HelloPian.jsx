5d93f924f394ff37c5475fbe4f319558b292e464
Pian <alifalpian157@gmail.com>
2025-07-19 14:36:30 +0700

    component

--- react-basic/src/hello-pian/HelloPian.jsx
+++ react-basic/src/hello-pian/HelloPian.jsx
@@ -1,7 +1,29 @@
 export default function HelloPian() {
    return (
       <div>
+         <Header />
+         <Pharagraphes />
+      </div>
+   )
+}
+// Komponen adalah fungsi yang mengembalikan elemen JSX.
+// Setiap komponen bisa digunakan kembali dan bisa menerima data lewat props.
+function Greeting(props) {
+  return <h1>Halo, {props.nama}!</h1>;
+}
+
+function Header() {
+   return (
+      <div>
          <h1>Hello Pian</h1>
+      </div>
+   )
+}
+// Komponen di React ibarat potongan UI yang bisa dipakai berulang.
+// Bisa dikasih data lewat props, dan hasilnya JSX (HTML di dalam JS).
+function Pharagraphes() {
+   return (
+      <div>
          <p>This is my first React App</p>
          <p>I hope you like it loplop</p>
       </div>
