090d0f8fcb50418bf84573ec4a7170aa39f784da
Pian <alifalpian157@gmail.com>
2025-07-19 17:35:38 +0700

    Null Component

--- react-basic/src/todo-list/Todo.jsx
+++ react-basic/src/todo-list/Todo.jsx
@@ -1,10 +1,12 @@
-export default function Todo({PropsText, isCompleted}) {
-  if (isCompleted) {
+export default function Todo({ PropsText, isCompleted, isDeleted = false}) {
+  if (isDeleted) {
+    return null;
+  } else if (isCompleted) {
     return (
       <li>
         <del>{PropsText}</del>
       </li>
-    )
+    );
   } else {
     return <li>{PropsText}</li>;
   }
--- react-basic/src/todo-list/TodoList.jsx
+++ react-basic/src/todo-list/TodoList.jsx
@@ -4,13 +4,9 @@ export default function TodoList() {
   return (
     <ul>
       <Todo PropsText="Belajar React" isCompleted={true} />
-      <Todo PropsText="Belajar Javascript" isCompleted={false} />
+      <Todo PropsText="Belajar Javascript" isDeleted={true} />
       <Todo PropsText="Belajar CSS" isCompleted={true} />
       <Todo PropsText="Belajar HTML" isCompleted={false} />
-      <Todo PropsText="Belajar PHP" isCompleted={true} />
-      <Todo PropsText="Belajar Python" isCompleted={false} />
-      <Todo PropsText="Belajar Java" isCompleted={true} />
-      <Todo PropsText="Belajar C++" isCompleted={false} />
     </ul>
   );
 }
