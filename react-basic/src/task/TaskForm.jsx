290582c2181e512349595722a6a691ef839cfc8a
Pian <alifalpian157@gmail.com>
2025-07-21 10:13:03 +0700

    Sharing State

---
+++ react-basic/src/task/TaskForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function TaskForm({ onSubmit }) {
+  const [item, setItem] = useState("");
+  function handleChange(e) {
+    setItem(e.target.value);
+  }
+  function handleClick(e) {
+    e.preventDefault();
+    onSubmit(item);
+    setItem("");
+  }
+  return (
+    <div>
+      <h1>Create Task</h1>
+      <form>
+        <input value={item} onChange={handleChange} />
+        <button onClick={handleClick}>Add Task</button>
+      </form>
+    </div>
+  );
+}
