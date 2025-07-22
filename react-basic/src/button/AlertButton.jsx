71abb890d9ed59664f7f8ba79df3f2f6ed35cb9b
Pian <alifalpian157@gmail.com>
2025-07-22 16:02:01 +0700

    ref

--- react-basic/src/button/AlertButton.jsx
+++ react-basic/src/button/AlertButton.jsx
@@ -1,8 +1,11 @@
-export default function AlertButton({ text, message }) {
-  function handleClick(e) {
-   console.info(e.target);
-    alert(message);
-  }
+import { useRef } from "react";
 
+export default function AlertButton({ text, message }) {
+  const counter = useRef(0);
+
+  function handleClick(e) {
+    console.info(e);
+    alert(`${message} ${counter.current++}`);
+  }
   return <button onClick={handleClick}>{text}</button>;
 }
